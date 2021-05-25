95df828d6d2b817f51911ccdea6fd032cc12b457
Takaki-Kasuga <takaki.kasuga@rakus-partners.co.jp>
2021-05-25 22:23:53 +0900

    React開発環境を構築

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+// import reducer from './reducers';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// const store = createStore(reducer)
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  // <Provider store={store}>
+  //   <App />
+  // </Provider>,
   document.getElementById('root')
 );
 
