46c42eb9b8de566cb1cc815adc282879faebf51f
Takaki-Kasuga <takaki.kasuga@rakus-partners.co.jp>
2021-05-27 18:13:49 +0900

    fiフォルダやファイルをインポートすれば通信できるように生成

---
+++ src/firestore/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAn0Q8q7YSv8Oi4OkbRY7sEz0HayGN4wfk",
+  authDomain: "rakuraku-react.firebaseapp.com",
+  projectId: "rakuraku-react",
+  storageBucket: "rakuraku-react.appspot.com",
+  messagingSenderId: "785600496904",
+  appId: "1:785600496904:web:041606a55648e46162d8bd"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase
