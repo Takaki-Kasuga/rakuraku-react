f2baa37fd66f87bd16758f78474326a8a3c28c62
miu-uim <miu.aizawa@rakus-partners.co.jp>
2021-05-28 15:15:18 +0900

    アカウントのテーブル・トップへ戻るボタン変更

--- src/components/UserAccount.js
+++ src/components/UserAccount.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useHistory, useHittory} from 'react-router-dom';
 
 //マテリアルUIテーブル
 import { withStyles, makeStyles } from '@material-ui/core/styles';
@@ -10,23 +11,74 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
+//マテリアルUIボタン
+import Button from '@material-ui/core/Button';
+import { Grid } from '@material-ui/core';
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+function createData(username, useremail) {
+  return { username,useremail};
+}
+
+const rows = [
+  createData('ユーザーの名前','ユーザーのメールアドレス')
+];
+
+const useStyles = makeStyles(() => ({
+    table: {
+        minWidth: 700,
+      },
+  }));
+
 const UserAccount =()=>{
+    const history = useHistory();
+    const classes = useStyles();
     return (
         <React.Fragment>
-            <div>
-                <h1>アカウント</h1>
-                <table border="1">
-                    <tr>
-                        <th>UserName</th>
-                        <th>UserEmail</th>
-                    </tr>
-                    <tr>
-                        <td>ログインユーザの名前が入ります</td>
-                        <td>ログインユーザーのメールアドレスが入ります</td>
-                    </tr>
-                </table>
-                <a href="/"><button>トップ画面を表示する</button></a>
-            </div>
+            <Grid alignItems="center" justify="center">
+            <TableContainer component={Paper}>
+                <Table className={classes.table} aria-label="customized table">
+                    <TableHead>
+                    <TableRow>
+                        <StyledTableCell>UserName</StyledTableCell>
+                        <StyledTableCell align="static">UserEmail</StyledTableCell>
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {rows.map((row) => (
+                        <StyledTableRow key={row.username}>
+                            <StyledTableCell component="th" scope="row">
+                                {row.username}
+                            </StyledTableCell>
+                            <StyledTableCell align="static">{row.useremail}</StyledTableCell>
+                        </StyledTableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            </Grid>
+                <Grid container alignItems="center" justify="center">
+                    <Grid> 
+                        <Button variant="outlined" color="default" onClick={()=>history.push('/')}>トップ画面を表示する</Button>
+                    </Grid> 
+                </Grid>
         </React.Fragment>
     )
 }
