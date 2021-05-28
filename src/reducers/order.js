60a5f0f067b588b3d73c790985e3abab5232bccd
“Toda-Atsuko” <atsuko.toda@rakus-partners.co.jp>
2021-05-28 17:26:28 +0900

    OrderHistoryの変更点を保存

--- src/reducers/order.js
+++ src/reducers/order.js
@@ -1,2 +1,6 @@
-import { SET_ITEMS } from "../actions/index";
+import React, { Component } from 'react';
+import {connnect} from 'react-redux'
 
+import { items } from '../actions/index';
+
+export const OrderHistory
