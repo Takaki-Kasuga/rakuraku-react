35f5415efde67d97fb1da0ab27ab97dd902a259b
miu-uim <miu.aizawa@rakus-partners.co.jp>
2021-05-28 14:26:57 +0900

    アカウント画面マテリアルUI導入

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,8 +13,6 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -42,17 +40,14 @@ const Header =()=>{
             <div className={classes.root}>
                 <AppBar position="static" color="inherit">
                     <Toolbar>
-                    {/* <IconButton edge="start" className={classes.menuButton} color="secondary" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton> */}
-                    <Typography variant="h6" className={classes.title}>
-                    <a href="/"><img src={logo} style={style}/></a>
-                    </Typography>
-                        <Button color="secondary" onClick={()=>history.push('/useraccount')}>アカウント</Button>
-                        <Button color="secondary" onClick={()=>history.push('/login')}>ログイン</Button>
-                        <Button color="secondary" onClick={() => { dispatch(logout()) }}>ログアウト</Button>
-                        <Button color="secondary"onClick={()=>history.push('/cartlist')}>ショッピングカート</Button>
-                        <Button color="secondary"onClick={()=>history.push('/orderhistory')}>注文履歴</Button>
+                        <Typography variant="h6" className={classes.title}>
+                        <a href="/"><img src={logo} style={style}/></a>
+                        </Typography>
+                            <Button color="secondary" onClick={()=>history.push('/useraccount')}>アカウント</Button>
+                            <Button color="secondary" onClick={()=>history.push('/login')}>ログイン</Button>
+                            <Button color="secondary" onClick={() => { dispatch(logout()) }}>ログアウト</Button>
+                            <Button color="secondary"onClick={()=>history.push('/cartlist')}>ショッピングカート</Button>
+                            <Button color="secondary"onClick={()=>history.push('/orderhistory')}>注文履歴</Button>
                     </Toolbar>
                 </AppBar>
             </div>
