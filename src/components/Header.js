98c2ace0b9635953bd9067cbcbeaf40dbf9a8005
miu-uim <miu.aizawa@rakus-partners.co.jp>
2021-05-28 13:38:03 +0900

    ヘッダーにマテリアルUIを適用

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,54 @@
 import React from 'react';
+
+//ロゴ画像
 import logo from '../img/header_logo.png'
 
+//マテリアルUIヘッダー
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
+  
 const Header =()=>{
+    const classes = useStyles();
+    const style = {
+        width :"20%",
+        height: "20%"
+    }
     return (
         <React.Fragment>
+            <div className={classes.root}>
+                <AppBar position="static" color="inherit">
+                    <Toolbar>
+                    {/* <IconButton edge="start" className={classes.menuButton} color="secondary" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton> */}
+                    <Typography variant="h6" className={classes.title}>
+                    <a href="/"><img src={logo} style={style}/></a>
+                    </Typography>
+                    <Button color="secondary">Login</Button>
+                    <Button color="secondary">Login</Button>
+                    <Button color="secondary">Login</Button>
+                    <Button color="secondary">Login</Button>
+                    </Toolbar>
+                </AppBar>
+            </div>
             <div>
-                <a href="/"><img src={logo}/></a>
                 <ul>
                     <li><a href="/useraccount">アカウント</a></li>
                     <li><a href="/login">ログイン</a></li>
