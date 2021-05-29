934d8aafda243c9b0d406627070c78ee31650f5c
Takaki-Kasuga <takaki.kasuga@rakus-partners.co.jp>
2021-05-29 12:42:51 +0900

    src/reducers/orders.jsファイルの追加、src/actions/index.jsとsrc/reducers/index.jsの修正でstoreのstateとして値を管理できるように変更

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,5 +3,6 @@ import itemState from './items'
 import toppingState from './toppings'
 import userIdState from './user'
 import selectedToppingState from './selectedToppings'
+import orderState from './orders'
 
-export default combineReducers({ itemState, toppingState, userIdState, selectedToppingState })
+export default combineReducers({ itemState, toppingState, userIdState, selectedToppingState, orderState })
