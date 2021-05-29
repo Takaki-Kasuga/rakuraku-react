e211e21ae844676850fe943b3103f27a5e921749
IHprogram <ihm20202020@gmail.com>
2021-05-29 12:47:17 +0900

    useStateを用いて新規登録画面の各入力欄の値を取得

--- src/components/RegisterEmail.js
+++ src/components/RegisterEmail.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, TextField } from '@material-ui/core';
 
 export const RegisterEmail = () => {
+  const [username, setUsername] = useState(''),
+    [email, setEmail] = useState(''),
+    [password, setPassword] = useState(''),
+    [confirmPassword, setConfirmPassword] = useState('');
+
+  const register = () => {
+    console.log(username);
+    console.log(email);
+    console.log(password);
+    console.log(confirmPassword);
+  }
+
+  const clear = () => {
+    setUsername('');
+    setEmail('');
+    setPassword('');
+    setConfirmPassword('');
+  }
+
+
   return (
     <React.Fragment>
       <h2>新規登録</h2>
@@ -11,29 +31,29 @@ export const RegisterEmail = () => {
           <label>
             名前:
           </label>
-          <TextField />
+          <TextField fullWidth={true} value={username} onChange={(e) => setUsername(e.target.value)} type={"text"} />
         </div>
         <div>
           <label>
             メールアドレス:
           </label>
-          <TextField />
+          <TextField fullWidth={true} value={email} onChange={(e) => setEmail(e.target.value)} type={"email"} />
         </div>
         <div>
           <label>
             パスワード(6文字以上):
           </label>
-          <TextField />
+          <TextField fullWidth={true} value={password} onChange={(e) => setPassword(e.target.value)} type={"password"} />
         </div>
         <div>
           <label>
             確認用パスワード:
           </label>
-          <TextField />
+          <TextField fullWidth={true} value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} type={"password"} />
         </div>
         <div>
-          <Button variant="contained" color="primary">登録</Button>
-          <Button variant="contained" color="primary">クリア</Button>
+          <Button variant="contained" color="primary" onClick={register}>登録</Button>
+          <Button variant="contained" color="primary" onClick={clear}>クリア</Button>
         </div>
       </form>
     </React.Fragment>
