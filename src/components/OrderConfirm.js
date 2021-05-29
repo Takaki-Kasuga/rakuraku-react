63a5287dac200f738c16db75c562f7a5d03b4f85
miu-uim <miu.aizawa@rakus-partners.co.jp>
2021-05-29 18:00:55 +0900

    注文確認画面の画面表示

--- src/components/OrderConfirm.js
+++ src/components/OrderConfirm.js
@@ -26,7 +26,7 @@ import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 
 //イメージ
-import image from '../img/1.jpg'
+import image from '../img/2.jpg'
 
 const OrderConfirm =()=>{
     // データリスト（テーブル）
@@ -40,7 +40,7 @@ const OrderConfirm =()=>{
         },
         // カード
         card: {
-        maxWidth: '600px',
+        maxWidth: '200px',
         },
         media: {
         height: 200,
@@ -48,16 +48,14 @@ const OrderConfirm =()=>{
         },
     });
 
-    function createData(id,itemName,image, itemCount, itemPrice, toppingPrice, smallCount) {
-        return { id,itemName,image, itemCount, itemPrice, toppingPrice, smallCount };
+    function createData(id,itemName,image, itemCount, itemPrice, toppingName,toppingPrice,smallTax, smallCount) {
+        return { id,itemName,image, itemCount, itemPrice, toppingName,toppingPrice, smallTax,smallCount };
     }
 
   const rows = [
+    createData(0,'ハワイアン・パラダイス',image,5,'M:350円','トッピング１',200,200,2200),
     createData(1,'https://firebasestorage.googleapis.com/v0/b/rakuraku-react.appspot.com/o/8.jpg?alt=media&token=5482ca98-4d73-493c-8a3e-e3bc5b7c7037', 159, 6.0, 24, 4.0),
     createData(2,'https://firebasestorage.googleapis.com/v0/b/rakuraku-react.appspot.com/o/8.jpg?alt=media&token=5482ca98-4d73-493c-8a3e-e3bc5b7c7037', 237, 9.0, 37, 4.3),
-    // createData('https://firebasestorage.googleapis.com/v0/b/rakuraku-react.appspot.com/o/8.jpg?alt=media&token=5482ca98-4d73-493c-8a3e-e3bc5b7c7037', 262, 16.0, 24, 6.0),
-    // createData('https://firebasestorage.googleapis.com/v0/b/rakuraku-react.appspot.com/o/8.jpg?alt=media&token=5482ca98-4d73-493c-8a3e-e3bc5b7c7037', 305, 3.7, 67, 4.3),
-    // createData('https://firebasestorage.googleapis.com/v0/b/rakuraku-react.appspot.com/o/8.jpg?alt=media&token=5482ca98-4d73-493c-8a3e-e3bc5b7c7037', 356, 16.0, 49, 3.9),
   ];
 
   const classes = useStyles();
@@ -71,50 +69,48 @@ const OrderConfirm =()=>{
                 <Table className={classes.table} aria-label="simple table">
                 <TableHead>
                     <TableRow>
-                    <TableCell>商品名</TableCell>
-                    <TableCell align="right">商品個数と値段（税抜き）</TableCell>
-                    <TableCell align="right">トッピングの値段（税抜き）</TableCell>
-                    <TableCell align="right">金額</TableCell>
-                    <TableCell align="right"></TableCell>
+                        {/* <tableCell>id</tableCell> */}
+                        <TableCell align="center">商品名</TableCell>
+                        <TableCell align="right">商品個数と値段（税抜き）</TableCell>
+                        <TableCell align="right">トッピングの値段（税抜き）</TableCell>
+                        <TableCell align="right">金額</TableCell>
+                        <TableCell align="right"></TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {rows.map(row => (
                     <TableRow key={row.id}>
-
                         <TableCell component="th" scope="row">
-
-                        <Card className={classes.card}>
-                            <CardContent>
-                                <Typography variant="body2" color="textSecondary" component="p">
-                                ハワイアンパラダイス
-                                </Typography>
-                            </CardContent>
-                            <CardMedia
-                            className={classes.media}
-                            image={row.image}
-                            title="Contemplative Reptile"
-                            />
-                        </Card>
-
+                            <Card className={classes.card}>
+                                <CardContent>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                    {row.itemName}
+                                    </Typography>
+                                </CardContent>
+                                <CardMedia
+                                className={classes.media}
+                                image={row.image}
+                                title="Contemplative Reptile"
+                                />
+                            </Card>
                         {/* <img src={row.name}></img> */}
                         </TableCell>
                         <TableCell align="right">
-                        <p>M:〜〜</p>
-                        <p>個数:〜個</p>
+                            <p>M:{row.itemPrice}円</p>
+                            <p>個数{row.itemCount}個</p>
                         </TableCell>
                         <TableCell align="right">
-                        <p>ハワイアンソルトM：200円</p>
-                        <p>ハワイアンソルトL：300円</p>
+                            <p>{row.toppingName}：{row.toppingPrice}円</p>
+                            <p>{row.toppingName}：{row.toppingPrice}円</p>
                         </TableCell>
                         <TableCell align="right">
-                        <p>消費税：</p>
-                        <p>金額：〜（税込）</p>
+                            <p>消費税：{row.smallTax}円</p>
+                            <p>金額：{row.smallCount}円（税込）</p>
                         </TableCell>
-                        <TableCell align="right"><Button variant="outlined" color="secondary">
-                        削除
-                        </Button>
-                        </TableCell>
+                            {/* <TableCell align="right"><Button variant="outlined" color="secondary">
+                            削除
+                            </Button>
+                            </TableCell> */}
                     </TableRow>
                     ))}
                 </TableBody>
