7f27a49dcb731adba4c6fd414b529d655328c2e4
IHprogram <ihm20202020@gmail.com>
2021-05-30 17:01:25 +0900

    ログアウト時にstoreのroutingJudgeを0に変更する処理を追加

--- src/reducers/routingJudge.js
+++ src/reducers/routingJudge.js
@@ -1,4 +1,4 @@
-import { CHANGE_ROUTING_STATUS } from '../actions/index'
+import { CHANGE_ROUTING_STATUS, CHANGE_ZERO_ROUTING_STATUS } from '../actions/index'
 
 const initialState = {
   routingJudge: 0
@@ -7,6 +7,9 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case CHANGE_ROUTING_STATUS:
+      state.routingJudge = action.routingJudge
+      return state;
+    case CHANGE_ZERO_ROUTING_STATUS:
       state.routingJudge = action.routingJudge
       return state;
     default:
