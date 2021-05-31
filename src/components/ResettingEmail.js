d918bf24a5902f81bd79366f3436ffd20aed9404
IHprogram <ihm20202020@gmail.com>
2021-05-31 10:46:22 +0900

    パスワード再設定画面のバリデーションを実装

--- src/components/ResettingEmail.js
+++ src/components/ResettingEmail.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, TextField } from '@material-ui/core';
 
 export const ResettingEmail = () => {
+  const errors = {
+    emailError: " ",
+  };
+
+  const [email, setEmail] = useState(''),
+    [errorMessage, setErrorMessage] = useState(errors),
+    [isDisabled, setIsDisabled] = useState(true);
+
+  const isDisabledCheck = () => {
+    if (errorMessage.emailError === "") {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  }
+
+  const inputEmail = (e) => {
+    const new_value = e.target.value;
+    setEmail(new_value)
+    if (new_value === "") {
+      console.log('空です')
+      errorMessage.emailError = "メールアドレスを入力してください"
+      isDisabledCheck();
+    } else if (new_value.indexOf("@") == -1) {
+      errorMessage.emailError = "メールアドレスの形式が不正です"
+      isDisabledCheck();
+    } else {
+      errorMessage.emailError = "";
+      isDisabledCheck();
+    }
+  };
+
   return (
     <React.Fragment>
       <h2>パスワード再設定</h2>
@@ -12,9 +44,10 @@ export const ResettingEmail = () => {
           <label>
             Email:
           </label>
-          <TextField />
+          <TextField fullWidth={true} value={email} type={"email"} onChange={inputEmail} />
+          <div><span>{errorMessage.emailError}</span></div>
         </div>
-        <Button variant="contained" color="primary">再設定メールを送る</Button>
+        <Button variant="contained" color="primary" disabled={isDisabled} >再設定メールを送る</Button>
       </form>
     </React.Fragment>
   )
