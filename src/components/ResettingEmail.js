53024c635e275b683bfeac210fa02e4ae1d34a7d
IHprogram <ihm20202020@gmail.com>
2021-05-31 12:04:35 +0900

    パスワードリセットメールを送信できた後の自動的な画面遷移を実装

--- src/components/ResettingEmail.js
+++ src/components/ResettingEmail.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import { Button, TextField } from '@material-ui/core';
 import firebase from '../firebase/firebase';
+import { useHistory } from "react-router-dom";
 
 export const ResettingEmail = () => {
+  const history = useHistory();
+
   const errors = {
     emailError: " ",
   };
@@ -36,10 +39,20 @@ export const ResettingEmail = () => {
     }
   };
 
+  const goHome = async () => {
+    let promise = new Promise(resolve => {
+      window.setTimeout(() => resolve('/'), 3000);
+    });
+
+    let result = await promise;
+    history.push(result);
+  }
+
   const passwordResetting = () => {
     firebase.auth().sendPasswordResetEmail(email)
       .then(result => {
         setResetMessage("再設定メールをお送り致しました。約3秒後にトップページに自動遷移致します。遷移しない場合は以下のボタンをクリックしてください。");
+        goHome();
       }).catch(error => {
         setResetMessage("メール送信に失敗しました。正しいメールアドレスをご入力ください。");
       })
