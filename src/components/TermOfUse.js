7c85a8a44b01ace2ed37242531366c781d8b080d
Takaki-Kasuga <takaki.kasuga@rakus-partners.co.jp>
2021-06-02 16:25:50 +0900

    修正（戻るボタンのルーティング先をログイン画面に変更）

--- src/components/TermOfUse.js
+++ src/components/TermOfUse.js
@@ -215,7 +215,7 @@ export const TermOfUse = () => {
           <div align="center">
 
             <Button tyle={{ width: '300px', margin: '0 auto' }} variant="contained" color="primary" disabled={!state.checkedA} onClick={() => {
-              handleLink('/')
+              handleLink('/login')
             }}>
               登録画面へ戻る
               </Button>
